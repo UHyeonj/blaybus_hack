d9e5be3d0b7989887bb8c187eb5c107697c1af6b
UHyeonj <128715793+UHyeonj@users.noreply.github.com>
2025-02-16 20:57:54 +0900

    asdf

--- src/pages/GoogleLogin.jsx
+++ src/pages/GoogleLogin.jsx
@@ -1,11 +1,12 @@
 import '../styles/GoogleLogin.css';
+import googleImg from './../assets/google-logo.png';
 
 function GoogleLogin() {
     const handleLogin = () => {
         window.location.href = "https://blaybus-glowup.com/oauth2/authorization/google?redirect_uri=https://uhyeon.blaybus-glowup.com/main&mode=login";
     };
     
-    const googleimg = './../assets/google-logo.png'; 
+    //const googleimg = './../assets/google-logo.png'; 
     
     return (
         <button 
@@ -13,7 +14,7 @@ function GoogleLogin() {
             onClick={handleLogin} 
             className='google_login'
         >
-        <img src={googleimg}/>구글로 시작하기
+        <img src={googleImg}/>구글로 시작하기
         </button>
     );
 }
