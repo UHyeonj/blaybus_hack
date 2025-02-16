83f9b685fe3b8e2db540f859ed21368d2370b351
UHyeonj <128715793+UHyeonj@users.noreply.github.com>
2025-02-16 20:41:18 +0900

    da

--- src/pages/GoogleLogin.jsx
+++ src/pages/GoogleLogin.jsx
@@ -2,7 +2,7 @@ import '../styles/GoogleLogin.css';
 
 function GoogleLogin() {
     const handleLogin = () => {
-        window.location.href = "https://blaybus-glowup.com/oauth2/authorization/google?redirect_uri=https://uhyeon.blaybus-glowup.com/&mode=login";
+        window.location.href = "https://blaybus-glowup.com/oauth2/authorization/google?redirect_uri=https://uhyeon.blaybus-glowup.com/main&mode=login";
     };
     
     const googleimg = './src/assets/google-logo.png'; 
