ff3fb9cc4242c230396421078ef3b71670d73a5d
UHyeonj <128715793+UHyeonj@users.noreply.github.com>
2025-02-17 17:37:16 +0900

    ㅁㄴㅇㄹ

--- src/pages/ReservationList.jsx
+++ src/pages/ReservationList.jsx
@@ -25,6 +25,16 @@ const ReservationList = () => {
         }
 
         console.log("Token from cookie: ", token);
+        console.log(
+          {
+            method: "GET",
+            headers: {
+              "Authorization": `Bearer ${token}`,
+              "Content-Type": "application/json"
+            },
+            credentials: 'include'
+          }
+        );
         const response = await fetch("https://blaybus-glowup.com/reservation/user", {
           method: "GET",
           headers: {
