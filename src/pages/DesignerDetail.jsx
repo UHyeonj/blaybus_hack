6a23112c4f1d78d382ce0f1cc5cf647ef0f2935e
Kwon <kdg5436@gmail.com>
2025-02-19 20:18:53 +0900

    콘솔 주석

--- src/pages/DesignerDetail.jsx
+++ src/pages/DesignerDetail.jsx
@@ -43,6 +43,8 @@ function DesignerDetail() {
 
     // selectedDate를 YYYY-MM-DD 형식으로 변환
     const formattedDate = selectedDate.toISOString().split("T")[0];
+
+    // 콘솔 확인
     console.log(`selectedDate : ${selectedDate}`);
     console.log(`formattedDate : ${formattedDate}`);
 
