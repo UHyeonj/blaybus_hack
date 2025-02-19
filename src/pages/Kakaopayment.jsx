0d87bc82ff82e876f9b2ace6c8c75acd9c9f3233
UHyeonj <dngusdlqwkd@gmail.com>
2025-02-19 11:49:28 +0900

    카카오페이먼트 css추가

--- src/pages/Kakaopayment.jsx
+++ src/pages/Kakaopayment.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import "../styles/Kakaopayment.css";
 
 function Kakaopayment() {
   const navigate = useNavigate();
@@ -24,7 +25,16 @@ function Kakaopayment() {
     navigate("/reservations");
   };
 
-  return <button onClick={handlePaymentSuccess}>결제 완료</button>;
+  return (
+    <div className="kakao_container">
+      <div className="kakao_state_container">
+        <h3>카카오페이지 결제 후</h3>
+        <h3>결제 완료 버튼을 눌러주세요</h3>  
+      </div>
+      <button className="kakao_btn" onClick={handlePaymentSuccess}>결제 완료</button>
+    </div>
+  
+  );
 }
 
 export default Kakaopayment;
